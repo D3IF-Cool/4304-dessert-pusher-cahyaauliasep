e7ee6a165f161787c6d6c95e21b5e5d5a3db1590
cahyaauliasep <cahyaauliasep@gmail.com>
2021-03-22 10:39:26 +0700

    first commit

--- app/src/main/java/org/d3if0088/lifecycle/MainActivity.kt
+++ app/src/main/java/org/d3if0088/lifecycle/MainActivity.kt
@@ -5,14 +5,63 @@ import android.os.Bundle
 import android.util.Log
 import timber.log.Timber
 
+const val KEY_REVENUE = "revenue_key"
+const val KEY_DESSERT_SOLD = "dessert_sold_key"
+
 class MainActivity : AppCompatActivity() {
+    private var revenue = 0
+    private var dessertSold = 0
+
+    private lateinit var binding: ActivityMainBinding
     private lateinit var dessertTimer: DessertTimer
+
+    data class Dessert(val imageId: Int, val price: Int, val startProductAmount: Int)
+
+    private val allDesserts = listOf(
+        Dessert(R.drawable.cupcake, 5,0 ),
+        Dessert(R.drawable.donut, 10, 5),
+        Dessert(R.drawable.eclair, 15, 20),
+        Dessert(R.drawable.froyo, 30, 50),
+        Dessert(R.drawable.gingerbread, 50, 100),
+        Dessert(R.drawable.honeycomb, 100, 200),
+        Dessert(R.drawable.icecreamsandwich, 500, 500),
+        Dessert(R.drawable.jellybean, 1000, 1000),
+        Dessert(R.drawable.kitkat, 2000, 2000),
+        Dessert(R.drawable.lollipop, 3000, 4000),
+        Dessert(R.drawable.marshmallow, 4000, 8000),
+        Dessert(R.drawable.nougat, 5000, 16000),
+        Dessert(R.drawable.oreo, 6000, 20000)
+    )
+    private var currentDessert = allDesserts[0]
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Timber.i("onCreate called")
+
         setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.dessertButton.setOnClickListener {
+            onDessertClicked()
+        }
+
         dessertTimer = DessertTimer(this.lifecycle)
+
+        if (savedInstanceState != null) {
+            revenue = savedInstanceState.getInt(KEY_REVENUE, 0)
+            dessertsSold = savedInstanceState.getInt(KEY_DESSERT_SOLD, 0)
+            showCurrentDessert()
+        }
+        binding.revenue = revenue
+        binding.amountSold = dessertsSold
+    }
+
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(KEY_REVENUE, revenue)
+        outState.putInt(KEY_DESSERT_SOLD, dessertsSold)
     }
 
     override fun onStart() {
