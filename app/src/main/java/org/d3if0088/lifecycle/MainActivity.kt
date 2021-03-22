47f74a27483601d5cbde30ff5aafb2307af0abc1
cahyaauliasep <cahyaauliasep@gmail.com>
2021-03-22 09:07:47 +0700

    3.1.Mempelajari method onCreate dan Logging

--- app/src/main/java/org/d3if0088/lifecycle/MainActivity.kt
+++ app/src/main/java/org/d3if0088/lifecycle/MainActivity.kt
@@ -2,10 +2,18 @@ package org.d3if0088.lifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("MainActivity", "onCreate Called")
+
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("MainActivity", "onStart Called")
     }
 }
